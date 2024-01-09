723a6d441f26b8bf18fe8ad5e3bdf7f0ec7e757d
Earnest Vince Quismundo <125753760+quismundo-earnestvince@users.noreply.github.com>
2024-01-09 19:08:30 +0800

    Update IntroScreen.js with Logo and Text Draft

--- IntroScreen.js
+++ IntroScreen.js
@@ -1,14 +1,37 @@
-import React from 'react';
-import { View, Image, StyleSheet } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, Image, StyleSheet, Text } from 'react-native';
 
-const IntroScreen = () => {
+const IntroScreen = ({ navigation }) => {
+  const [showLogo, setShowLogo] = useState(false);
+
+  useEffect(() => {
+    const timer1 = setTimeout(() => {
+      setShowLogo(true);
+    }, 1000);
+
+    const timer2 = setTimeout(() => {
+      navigation.navigate('Login');
+    }, 4000);
+
+    return () => {
+      clearTimeout(timer1);
+      clearTimeout(timer2);
+    };
+  }, [navigation]);
+
   return (
     <View style={styles.container}>
       <Image
-        source={require('./assets/animation.gif')}
-        style={styles.gifImage}
+        source={require('../assets/animation.gif')}
+        style={[styles.gifImage, !showLogo && { opacity: 0 }]}
         resizeMode="contain"
       />
+      {showLogo && (
+        <View style={styles.logoContainer}>
+          <Image source={require('../assets/logo.png')} style={styles.logo} />
+          <Text style={styles.text}>Notepad</Text>
+        </View>
+      )}
     </View>
   );
 };
@@ -23,7 +46,28 @@ const styles = StyleSheet.create({
   gifImage: {
     width: 500,
     height: 1000,
+    position: 'absolute',
+    top: 0,
+    left: 0,
+  },
+  logoContainer: {
+    position: 'absolute',
+    top: '10%', // Adjust the positioning as needed
+    left: '8%',
+    transform: [{ translateX: -50 }, { translateY: -50 }],
+    alignItems: 'center',
+  },
+  logo: {
+    width: 400,
+    height: 400,
+  },
+  text: {
+    textAlign: 'center',
+    fontSize: 40,
+    fontFamily: 'JimNightshade-Regular',
   },
 });
 
 export default IntroScreen;
+
+
