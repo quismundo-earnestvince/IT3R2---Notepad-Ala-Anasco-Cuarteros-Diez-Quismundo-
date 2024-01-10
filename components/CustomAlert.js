a1a78432e03d452e0a6c3c7c3bb939dfcd8db97e
Earnest Vince Quismundo <125753760+quismundo-earnestvince@users.noreply.github.com>
2024-01-11 00:54:50 +0800

    Dependent Text Display

--- components/CustomAlert.js
+++ components/CustomAlert.js
@@ -2,11 +2,15 @@ import React from 'react';
 import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
 import Modal from 'react-native-modal';
 
-const CustomAlert = ({ isVisible, closeAlert }) => {
+const CustomAlert = ({ isVisible, closeAlert, messageType }) => {
+  const getTitleText = () => {
+    return messageType === 'added' ? 'Note Added!' : 'Note Updated!';
+  };
+
   return (
     <Modal isVisible={isVisible}>
       <View style={styles.modalContainer}>
-        <Text style={styles.title}>Note Updated!</Text>
+        <Text style={styles.title}>{getTitleText()}</Text>
         <TouchableOpacity style={styles.button} onPress={closeAlert}>
           <Text style={styles.buttonText}>OK</Text>
         </TouchableOpacity>
@@ -21,7 +25,7 @@ const styles = StyleSheet.create({
     padding: 20,
     borderRadius: 10,
     alignItems: 'center',
-    width:300,
+    width: 300,
     alignSelf: 'center',
   },
   title: {
@@ -33,7 +37,7 @@ const styles = StyleSheet.create({
     fontSize: 16,
     fontWeight: 'bold',
     color: 'blue',
-    marginTop:20,
+    marginTop: 20,
   },
 });
 
