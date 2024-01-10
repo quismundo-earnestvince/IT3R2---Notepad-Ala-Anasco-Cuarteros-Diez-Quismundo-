d9fa543bf2ff8b8203f3d6c97e3fba70c6ad7bd1
Earnest Vince Quismundo <125753760+quismundo-earnestvince@users.noreply.github.com>
2024-01-11 00:31:45 +0800

    Create CustomAlert.js

---
+++ components/CustomAlert.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import Modal from 'react-native-modal';
+
+const CustomAlert = ({ isVisible, closeAlert }) => {
+  return (
+    <Modal isVisible={isVisible}>
+      <View style={styles.modalContainer}>
+        <Text style={styles.title}>Note Updated!</Text>
+        <TouchableOpacity style={styles.button} onPress={closeAlert}>
+          <Text style={styles.buttonText}>OK</Text>
+        </TouchableOpacity>
+      </View>
+    </Modal>
+  );
+};
+
+const styles = StyleSheet.create({
+  modalContainer: {
+    backgroundColor: 'white',
+    padding: 20,
+    borderRadius: 10,
+    alignItems: 'center',
+    width:300,
+    alignSelf: 'center',
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+  buttonText: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: 'blue',
+    marginTop:20,
+  },
+});
+
+export default CustomAlert;
